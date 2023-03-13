ae956d49e55c754b80a32cb9ccde101f8d1e8c98
Aleksey Arabchik <aleksey-090@mail.ru>
2023-03-13 19:55:20 +0300

    project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { IPost } from "./types/Posts";
 
 const App = () => {
-  return <div></div>;
+  const [posts, setPosts] = useState<IPost[]>([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    fetchPosts();
+  });
+
+  async function fetchPosts() {
+    try {
+      setLoading(true);
+      const response = await axios.get<IPost[]>(
+        "https://studapi.teachmeskills.by/blog/posts/?limit=20"
+      );
+      setPosts(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading</h1>
+      ) : (
+        <div>
+          {posts.map((post) => (
+            <div key={post.id}>
+              {post.image} {post.title}{" "}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
